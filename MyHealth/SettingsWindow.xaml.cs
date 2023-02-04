86beb54c2070f35de47c60cc5e22dc4a3f2b2cdf
nezhadian <yasin.ebr.nezh@gmail.com>
2023-02-05 00:46:13 +0330

    Arrow Keys Implemented

--- MyHealth/SettingsWindow.xaml.cs
+++ MyHealth/SettingsWindow.xaml.cs
@@ -17,9 +17,53 @@ namespace MyHealth
     /// </summary>
     public partial class SettingsWindow : Window
     {
+
+        public static RoutedCommand ArrowUp = new RoutedCommand("ArrowUp",typeof(SettingsWindow));
+        public static RoutedCommand ArrowDown = new RoutedCommand("ArrowDown",typeof(SettingsWindow));
+
         public SettingsWindow()
         {
             InitializeComponent();
+        }
+
+        private void ArrowKeys_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (lstItems.SelectedIndex == -1)
+                return;
+
+            if(ArrowUp == e.Command)
+            {
+                e.CanExecute = lstItems.SelectedIndex > 0;
+            }
+            else if (ArrowDown == e.Command)
+            {
+                e.CanExecute = lstItems.SelectedIndex < lstItems.Items.Count - 1;
+            }
+        }
+        private void ArrowKeys_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (ArrowUp == e.Command)
+            {
+                int index = lstItems.SelectedIndex;
+                int target = index - 1;
+                object a = lstItems.SelectedItem;
+                lstItems.Items.RemoveAt(index);
+                lstItems.Items.Insert(target, a);
+                lstItems.SelectedIndex = target;
+                lstItems.Focus();
+
+
+            }
+            else if (ArrowDown == e.Command)
+            {
+                int index = lstItems.SelectedIndex;
+                int target = index + 1;
+                object a = lstItems.SelectedItem;
+                lstItems.Items.RemoveAt(index);
+                lstItems.Items.Insert(target, a);
+                lstItems.SelectedIndex = target;
+                lstItems.Focus();
+            }
         }
     }
 }
