fc9cbf5c0208767a80269e8e4e94870672635fe0
nezhadian <yasin.ebr.nezh@gmail.com>
2023-02-24 08:31:25 +0330

    DataAccess Added

---
+++ MyHealth/Classes/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHealth
+{
+    public class DataAccess
+    {
+        public static StepData[] Steps;
+
+        public static ITimerSlice[] GenerateSteps()
+        {
+            List<ITimerSlice> steps = new List<ITimerSlice>();
+            foreach (StepData item in Steps)
+            {
+                var step = item.ToStep();
+                if (step != null)
+                    steps.Add(step);
+            }
+            return steps.ToArray();
+        }
+
+        static DataAccess()
+        {
+            Steps = Templates.TemplateDictionary["standard"];
+        }
+
+    }
+}
