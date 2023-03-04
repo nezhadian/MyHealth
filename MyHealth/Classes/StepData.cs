7f18703b6b02e1448c31b7b2308a9f9242e4929d
nezhadian <yasin.ebr.nezh@gmail.com>
2023-03-04 18:48:08 +0330

    Add DependencyProperties to StepData

--- MyHealth/Classes/StepData.cs
+++ MyHealth/Classes/StepData.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 
 namespace MyHealth
 {
-    public class StepData : IXmlSerializable
+    public class StepData : ListBoxItem,IXmlSerializable
     {
         public enum StepTypes
         {
@@ -20,10 +22,39 @@ namespace MyHealth
             Body,Eye
         }
 
-        public StepTypes StepType;
-        public string StepName;
-        public TimeSpan Duration;
-        public ImageListes ImageList;
+        #region Dependency Properites
+        public static readonly DependencyProperty StepTypeProperty =
+            DependencyProperty.Register("StepType", typeof(StepTypes), typeof(StepData), new PropertyMetadata());
+        public static readonly DependencyProperty StepNameProperty =
+            DependencyProperty.Register("StepName", typeof(string), typeof(StepData), new PropertyMetadata());
+        public static readonly DependencyProperty DurationProperty =
+            DependencyProperty.Register("Duration", typeof(TimeSpan), typeof(StepData), new PropertyMetadata());
+        public static readonly DependencyProperty ImageListProperty =
+            DependencyProperty.Register("ImageList", typeof(ImageListes), typeof(StepData), new PropertyMetadata());
+
+        #endregion
+
+        public StepTypes StepType
+        {
+            get { return (StepTypes)GetValue(StepTypeProperty); }
+            set { SetValue(StepTypeProperty, value); }
+        }
+        public string StepName
+        {
+            get { return (string)GetValue(StepNameProperty); }
+            set { SetValue(StepNameProperty, value); }
+        }
+        public TimeSpan Duration
+        {
+            get { return (TimeSpan)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+        public ImageListes ImageList
+        {
+            get { return (ImageListes)GetValue(ImageListProperty); }
+            set { SetValue(ImageListProperty, value); }
+        }
+
 
         public override string ToString()
         {
