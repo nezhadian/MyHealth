3a4cec09a683f08017d767d5eef53acc3653bcf7
nezhadian <yasin.ebr.nezh@gmail.com>
2023-03-04 21:36:07 +0330

    StepDataIcon Converter Implemented

---
+++ MyHealth/Classes/Converter/StepDataIconConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Shapes;
+
+namespace MyHealth
+{
+
+    class StepDataIconConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            object resource;
+            if ((resource = App.Current.TryFindResource($"{values[0]}.{values[1]}")) != null)
+                return resource;
+            if ((resource = App.Current.TryFindResource($"{values[0]}")) != null)
+                return resource;
+            return Binding.DoNothing;
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
