9dfd357875bd3f7809b2e0bb7d1d6f495032daae
nezhadian <yasin.ebr.nezh@gmail.com>
2023-03-04 22:52:39 +0330

    TimeSpan Implemented to StepData

---
+++ MyHealth/Classes/Converter/TimeSpanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MyHealth
+{
+    class TimeSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan)
+            {
+                return ((TimeSpan)value).ToString("hh':'mm':'ss");
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
