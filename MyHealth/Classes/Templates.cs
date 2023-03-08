ccba7afadd3d103203e6ea29401834617f8668e1
nezhadian <yasin.ebr.nezh@gmail.com>
2023-03-09 00:09:35 +0330

    Use ShortBreak Instead ImageSlider in LongBreaks

--- MyHealth/Classes/Templates.cs
+++ MyHealth/Classes/Templates.cs
@@ -27,13 +27,13 @@ namespace MyHealth
                         new StepData(){StepType = StepData.StepTypes.Seperator},
 
                         new StepData(){StepName = "25min Work", StepType = StepData.StepTypes.WorkTime,Duration = new TimeSpan(0,25,0)},
-                        new StepData(){StepName = "20min Long Break", StepType = StepData.StepTypes.ImageSlider,ImageList =  StepData.ImageListes.Body,Duration = new TimeSpan(0,20,0)},
+                        new StepData(){StepName = "20min Long Break", StepType = StepData.StepTypes.ShortBreak,Duration = new TimeSpan(0,20,0)},
                         new StepData(){StepName = "Ready For New Pomoduro", StepType = StepData.StepTypes.FreshStart},
             }},
 
             {"standard" , new StepData[]{
                         new StepData(){StepName = "52min Work",StepType = StepData.StepTypes.WorkTime,Duration = new TimeSpan(0,52,0)},
-                        new StepData(){StepName = "27min Resting",StepType = StepData.StepTypes.ImageSlider,ImageList =  StepData.ImageListes.Body,Duration = new TimeSpan(0,17,0)},
+                        new StepData(){StepName = "27min Resting",StepType = StepData.StepTypes.ShortBreak,Duration = new TimeSpan(0,17,0)},
                         new StepData(){StepName = "Ready",StepType = StepData.StepTypes.FreshStart},
                         }},
         };
