295cd530cf2632154e31e7470d90e732e66cc9d1
nezhadian <yasin.ebr.nezh@gmail.com>
2023-03-12 10:48:10 +0330

    ICanSaveSettingMenuItem Inteface Added

---
+++ MyHealth/Windows/ICanSaveSettingMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHealth
+{
+    public interface ICanSaveSettingMenuItem
+    {
+        public bool CanSave { get; }
+        public bool IsChanged { get; set; }
+        public void Save();
+    }
+}
