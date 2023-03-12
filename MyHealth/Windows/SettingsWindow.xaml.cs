357680716302d461e245f1aaf8386f7aae9f07de
nezhadian <yasin.ebr.nezh@gmail.com>
2023-03-12 10:49:34 +0330

    Setting Window Saveing System Implemented Setting Window MenuItems Added

--- MyHealth/Windows/SettingsWindow.xaml.cs
+++ MyHealth/Windows/SettingsWindow.xaml.cs
@@ -17,9 +17,55 @@ namespace MyHealth
     /// </summary>
     public partial class SettingsWindow : Window
     {
+        List<SettingListMenuItem> MenuItems = new List<SettingListMenuItem>()
+        {
+            new SettingListMenuItem("General",new GeneralSettingPage()),
+            new SettingListMenuItem("Step Editor",new StepEditorPage()),
+            new SettingListMenuItem("About Me",new AboutMePage()),
+        };
+
         public SettingsWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadMenuItems();
+        }
+
+        private void LoadMenuItems()
+        {
+            lstMenu.Items.Clear();
+            lstMenu.ItemsSource = MenuItems;
+        }
+
+        private void Save_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            foreach (var item in MenuItems)
+            {
+                if(item.ItemPage is ICanSaveSettingMenuItem)
+                {
+                    ICanSaveSettingMenuItem canSave = (ICanSaveSettingMenuItem)item.ItemPage;
+                    if (canSave.IsChanged &&!canSave.CanSave)
+                        return;
+                }
+            }
+            e.CanExecute = true;
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            foreach (var item in MenuItems)
+            {
+                if (item.ItemPage is ICanSaveSettingMenuItem)
+                {
+                    ICanSaveSettingMenuItem canSave = (ICanSaveSettingMenuItem)item.ItemPage;
+                    if (canSave.IsChanged)
+                        ((ICanSaveSettingMenuItem)item.ItemPage).Save();
+                }
+            }
+            DialogResult = true;
         }
     }
 }
