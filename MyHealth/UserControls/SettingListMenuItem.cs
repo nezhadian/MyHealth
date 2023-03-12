1721554985e34c9fdae288e00b4b3ba067f54387
nezhadian <yasin.ebr.nezh@gmail.com>
2023-03-12 09:05:29 +0330

    SettingListMenuItem Added

---
+++ MyHealth/UserControls/SettingListMenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyHealth
+{
+    public class SettingListMenuItem : ListBoxItem
+    {
+
+
+        public object ItemPage
+        {
+            get { return (object)GetValue(ItemPageProperty); }
+            set { SetValue(ItemPageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemPageProperty =
+            DependencyProperty.Register("ItemPage", typeof(object), typeof(SettingListMenuItem), new PropertyMetadata());
+
+        public SettingListMenuItem() { }
+        public SettingListMenuItem(string name,Page page)
+        {
+            Content = name;
+            ItemPage = page;
+        }
+
+
+    }
+}
