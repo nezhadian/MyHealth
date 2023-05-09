31b223c23253d64ea5a030e278432f783668fac8
nezhadian <yasin.ebr.nezh@gmail.com>
2023-05-09 12:46:43 +0430

    StepName Removed From StepData

--- MyHealth/Classes/StepData.cs
+++ MyHealth/Classes/StepData.cs
@@ -15,8 +15,6 @@ namespace MyHealth
         #region Dependency Properites
         public static readonly DependencyProperty StepTypeProperty =
             DependencyProperty.Register("StepType", typeof(StepTypes), typeof(StepData), new PropertyMetadata());
-        public static readonly DependencyProperty StepNameProperty =
-            DependencyProperty.Register("StepName", typeof(string), typeof(StepData), new PropertyMetadata());
         public static readonly DependencyProperty DurationProperty =
             DependencyProperty.Register("Duration", typeof(TimeSpan), typeof(StepData), new PropertyMetadata());
         public static readonly DependencyProperty ImageListProperty =
@@ -44,11 +42,6 @@ namespace MyHealth
             get { return (StepTypes)GetValue(StepTypeProperty); }
             set { SetValue(StepTypeProperty, value); }
         }
-        public string StepName
-        {
-            get { return (string)GetValue(StepNameProperty); }
-            set { SetValue(StepNameProperty, value); }
-        }
         public TimeSpan Duration
         {
             get { return (TimeSpan)GetValue(DurationProperty); }
@@ -58,45 +51,6 @@ namespace MyHealth
         {
             get { return (ImageListes)GetValue(ImageListProperty); }
             set { SetValue(ImageListProperty, value); }
-        }
-
-        public override string ToString()
-        {
-            switch (StepType)
-            {
-                case StepTypes.WorkTime:
-                case StepTypes.ShortBreak:
-                    return $"{StepName} | {Duration:hh':'mm':'ss}";
-                case StepTypes.ImageSlider:
-                    return $"{StepName} | ({ImageList}) {Duration:hh':'mm':'ss}";
-                case StepTypes.FreshStart:
-                    return $"{StepName}";
-                default:
-                    return "";
-            }
-        }
-        internal ITimerSlice ToStep()
-        {
-            switch (StepType)
-            {
-                case StepTypes.WorkTime:
-                    return new WorkTime(Duration);
-                case StepTypes.ImageSlider:
-                    switch (ImageList)
-                    {
-                        case ImageListes.Body:
-                            return new ImageSlider(Duration, System.IO.Path.Combine(Environment.CurrentDirectory, "Images", "body")) { StepName = StepName };
-                        case ImageListes.Eye:
-                            return new ImageSlider(Duration, System.IO.Path.Combine(Environment.CurrentDirectory, "Images", "eye")) { StepName = StepName };
-                    }
-                    return null;
-                case StepTypes.FreshStart:
-                    return new FreshStart() { StepName = StepName };
-                case StepTypes.ShortBreak:
-                    return new ShortBreak(Duration) { StepName = StepName };
-                default:
-                    return null;
-            }
         }
     }
 }
