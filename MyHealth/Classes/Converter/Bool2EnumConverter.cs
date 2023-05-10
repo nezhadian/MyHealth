a3c49507fe4d589699dc8fa9453398259e7b0e70
nezhadian <yasin.ebr.nezh@gmail.com>
2023-05-10 20:42:00 +0430

    Change TaskView Style For Grouping Property

---
+++ MyHealth/Classes/Converter/Bool2EnumConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MyHealth
+{
+    class Bool2EnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() == parameter.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(Enum.TryParse(targetType,parameter.ToString(),out object resault))
+            {
+                return resault;
+            }
+            else
+            {
+                return Binding.DoNothing;
+            }
+        }
+    }
+}
