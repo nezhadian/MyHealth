0194896857b9296b1afa631f4bb888bb38bcb269
nezhadian <yasin.ebr.nezh@gmail.com>
2023-05-14 21:00:45 +0430

    Switch Between Step And Task Text in Caption Text Hide Caption When In Clickable Steps Bold Timer

---
+++ MyHealth/Classes/Converter/CaptionTextSwitchConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MyHealth
+{
+    class CaptionTextSwitchConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values[0] is StepData step)
+            {
+                if(values[1] is string taskTitle)
+                {
+                    return step.StepType == StepData.StepTypes.WorkTime ? taskTitle : step.StepName;
+                }
+                else
+                {
+                    return step.StepName;
+                }
+            }
+            return Binding.DoNothing;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyHealth/Classes/Converter/Enum2VisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MyHealth
+{
+    class Enum2VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] splitVisibleValues = parameter.ToString().Split(",");
+            return splitVisibleValues.Contains(value.ToString()) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
