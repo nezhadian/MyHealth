8987f256ab25d1b5c83f83363b9dfcde9068f90b
nezhadian <yasin.ebr.nezh@gmail.com>
2023-05-15 19:43:53 +0430

    Bind SelectedStep To Frame With Converter To Show Relevant Content

---
+++ MyHealth/Classes/Converter/StepData2PageConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MyHealth
+{
+    class StepData2PageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is StepData step)
+            {
+                switch (step.StepType)
+                {
+                    case StepData.StepTypes.ImageSlider:
+                        return new ImageSlider(step);
+                    case StepData.StepTypes.FreshStart:
+                        return new FreshStart(step) ;
+                    case StepData.StepTypes.ShortBreak:
+                        return new ShortBreak(step);
+                    default:
+                        return null;
+                }
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
