349c98bdbec0d92d9c6a5d246221105d595da347
nezhadian <yasin.ebr.nezh@gmail.com>
2023-05-16 07:54:25 +0430

    List Index Plus One Converter Added

---
+++ MyHealth/Classes/Converter/ListIndexPlusOneConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MyHealth
+{
+    class ListIndexPlusOneConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is int index)
+            {
+                return index + 1;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
