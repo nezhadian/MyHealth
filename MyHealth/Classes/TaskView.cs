eab3a152689c6b72c784f41f2f91b3d7338b4d31
nezhadian <yasin.ebr.nezh@gmail.com>
2023-05-24 18:24:32 +0430

    TaskView Properties Changed Event

--- MyHealth/Classes/TaskView.cs
+++ MyHealth/Classes/TaskView.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 
 namespace MyHealth
 {
-    public class TaskView : DependencyObject
+    public class TaskView : DependencyObject,INotifyPropertyChanged
     {
+
         #region DependencyProperties
         public static readonly DependencyProperty TitleProperty =
-            DependencyProperty.Register("Title", typeof(string), typeof(TaskView), new PropertyMetadata());
+            DependencyProperty.Register("Title", typeof(string), typeof(TaskView), new PropertyMetadata("",OnTitleChanged));
+
         public static readonly DependencyProperty GroupingProperty =
-                    DependencyProperty.Register("Grouping", typeof(TaskGroups), typeof(TaskView), new PropertyMetadata(TaskGroups.Tasks));
+                    DependencyProperty.Register("Grouping", typeof(TaskGroups), typeof(TaskView), new PropertyMetadata(TaskGroups.Tasks,OnGroupingChanged));
+
+        
+
         #endregion
 
         public enum TaskGroups
@@ -31,7 +37,20 @@ namespace MyHealth
             set { SetValue(GroupingProperty, value); }
         }
 
-        
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TaskView taskView = (TaskView)d;
+            taskView.PropertyChanged?.Invoke(taskView, new PropertyChangedEventArgs("Title"));
+        }
+        private static void OnGroupingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TaskView taskView = (TaskView)d;
+            taskView.PropertyChanged?.Invoke(taskView, new PropertyChangedEventArgs("Grouping"));
+        }
 
     }
 }
