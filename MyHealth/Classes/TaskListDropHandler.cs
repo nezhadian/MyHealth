a54911e70b127decc2840ae803ba711c8536ee3c
nezhadian <yasin.ebr.nezh@gmail.com>
2023-05-30 10:31:35 +0430

    Dragdrop Issuse in Grouping Listboxes Fixed: When i drag and drop a listbox  with grouping the sorted list with source observable collection is different and this different make insert index confilit in resault make confilict behaviour of droping

--- MyHealth/Classes/TaskListDropHandler.cs
+++ MyHealth/Classes/TaskListDropHandler.cs
@@ -1,29 +1,56 @@
 ﻿using GongSolutions.Wpf.DragDrop;
+using GongSolutions.Wpf.DragDrop.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
 namespace MyHealth
 {
-    public class TaskListDropHandler : IDropTarget
+    public class TaskListDropHandler : DefaultDropHandler
     {
-        public void DragOver(IDropInfo dropInfo)
+        public override void DragOver(IDropInfo dropInfo)
         {
-            GongSolutions.Wpf.DragDrop.DragDrop.DefaultDropHandler.DragOver(dropInfo);
+            base.DragOver(dropInfo);
             if (dropInfo.TargetGroup == null)
             {
                 dropInfo.Effects = DragDropEffects.None;
             }
         }
 
-        public void Drop(IDropInfo dropInfo)
+        public override void Drop(IDropInfo dropInfo)
         {
-            GongSolutions.Wpf.DragDrop.DragDrop.DefaultDropHandler.Drop(dropInfo);
-
-            var data = DefaultDropHandler.ExtractData(dropInfo.Data).OfType<TaskView>().ToList();
+            var data = ExtractData(dropInfo.Data).OfType<TaskView>().ToList();
             foreach (var groupedItem in data)
             {
                 groupedItem.Grouping = (TaskView.TaskGroups)dropInfo.TargetGroup.Name;
             }
+
+            MatchSourceListWithSortedList(dropInfo.TargetCollection);
+            base.Drop(dropInfo);
         }
+
+        private void MatchSourceListWithSortedList(IEnumerable targetCollection)
+        {
+            if(targetCollection is ListCollectionView collectionView){
+                var sourceList = collectionView.TryGetList();
+                var sortedList = collectionView.Cast<object>().ToList();
+
+                int i = 0;
+                foreach (var item in sortedList)
+                {
+                    if (sortedList[i] != item)
+                        Move(sourceList, sourceList.IndexOf(item), i);
+                    i++;
+                }
+
+            }
+        }
+
+
     }
 
 
