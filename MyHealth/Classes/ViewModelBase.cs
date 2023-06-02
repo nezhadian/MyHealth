52139fff030d2bbf15b6f75255347efbc560769b
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-02 19:39:49 +0430

    ViewModelBase Class Added

---
+++ MyHealth/Classes/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MyHealth
+{
+    public abstract class ViewModelBase: INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Implamentation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public ViewModelBase()
+        {
+
+        }
+    }
+}
