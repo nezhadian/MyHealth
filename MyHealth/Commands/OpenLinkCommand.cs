d80b3741c5b81e88567a9910e4e3b8dba1a1b26e
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-04 20:06:49 +0430

    Social Link Card Button : Added

---
+++ MyHealth/Commands/OpenLinkCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace MyHealth
+{
+    public class OpenLinkCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public static OpenLinkCommand Instance;
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter is Uri)
+                return true;
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            Process.Start(new ProcessStartInfo(parameter.ToString()) { UseShellExecute = true });
+        }
+
+        static OpenLinkCommand()
+        {
+            Instance = new OpenLinkCommand();
+        }
+    }
+}
---
+++ MyHealth/Controls/SocialLinkCardButton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyHealth
+{
+    /// <summary>
+    /// Interaction logic for SocialLinkCardButton.xaml
+    /// </summary>
+    public class SocialLinkCardButton : Button
+    {
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(SocialLinkCardButton), new PropertyMetadata());
+
+
+        public Geometry IconPathData
+        {
+            get { return (Geometry)GetValue(IconPathDataProperty); }
+            set { SetValue(IconPathDataProperty, value); }
+        }
+        public static readonly DependencyProperty IconPathDataProperty =
+            DependencyProperty.Register("IconPathData", typeof(Geometry), typeof(SocialLinkCardButton), new PropertyMetadata());
+
+
+
+        public Uri Link
+        {
+            get { return (Uri)GetValue(LinkProperty); }
+            set { SetValue(LinkProperty, value); }
+        }
+        public static readonly DependencyProperty LinkProperty =
+            DependencyProperty.Register("Link", typeof(Uri), typeof(SocialLinkCardButton), new PropertyMetadata());
+    }
+}
