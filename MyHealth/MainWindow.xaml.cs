727ea5fb0a80f9d28ea015cd2986db57aa52d2d5
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-04 12:10:39 +0430

    Main Window : Timer Property Removed Instead TimerControl Used

--- MyHealth/MainWindow.xaml.cs
+++ MyHealth/MainWindow.xaml.cs
@@ -26,55 +26,23 @@ namespace MyHealth
     {
         public static TaskbarIcon TaskBarIcon => ((MainWindow)App.Current.MainWindow).tbNotify;
 
-        #region Dp
-        public static readonly DependencyProperty TimerProperty =
-            DependencyProperty.Register("Timer", typeof(Timer), typeof(MainWindow), new PropertyMetadata());
-        #endregion
-        #region Properties
-        //Task List
         public TaskListViewModel TaskListViewModel { get; set; }
-
-        //Step List
         public StepListViewModel StepListViewModel { get; set; }
 
-        //Timer
-        public Timer Timer
-        {
-            get { return (Timer)GetValue(TimerProperty); }
-            set { SetValue(TimerProperty, value); }
-        }
-        #endregion
-
-        //Ctor
         public MainWindow()
         {
             TaskListViewModel = new TaskListViewModel();
             StepListViewModel = new StepListViewModel();
-
-            InitalizeTimer();
             
             DataContext = this;
             InitializeComponent();
         }
 
-        #region Timer Initialization
-        private void InitalizeTimer()
-        {
-            Timer = new Timer();
-            Timer.Completed += Timer_Completed;
-            BindingOperations.SetBinding(Timer, Timer.DurationProperty, new Binding("SelectedStep.Duration") { Source = StepListViewModel });
-        }
         private void Timer_Completed(object sender, RoutedEventArgs e)
         {
             StepListViewModel.GoToNextStep();
         }
 
-        #endregion
-        #region Menu Events
-        private void mnuReset_Click(object sender, RoutedEventArgs e)
-        {
-            Timer.Reset();
-        }
         private void mnuSettings_Click(object sender, RoutedEventArgs e)
         {
             SettingsWindow win = new SettingsWindow();
@@ -85,10 +53,7 @@ namespace MyHealth
             tbNotify.Visibility = Visibility.Collapsed;
             Environment.Exit(0);
         }
-        #endregion
 
-        #region New Task Command And Textbox
-        //Add New Task When Enter Pressed        
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -100,10 +65,13 @@ namespace MyHealth
                 }
             }
         }
-        #endregion
+        private void PagesContent_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (StepListViewModel.ClickCommand.CanExecute(null))
+                StepListViewModel.ClickCommand.Execute(null);
+        }
 
         #region TaskBar Icon ContextMenu
-        //ContetMenu Init
         private void ContextMenu_Loaded(object sender, RoutedEventArgs e) => ContextMenuBindings();
         private void ContextMenuBindings()
         {
@@ -114,7 +82,6 @@ namespace MyHealth
             });
         }
 
-        //Items Events
         private void Open_Click(object sender, RoutedEventArgs e)
         {
             Activate();
@@ -123,6 +90,5 @@ namespace MyHealth
 
 
         #endregion
-        
     }
 }
