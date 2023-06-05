6f6851b91f872368ce9f8841113b98bdeeedf834
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-06 01:58:08 +0430

    TaskList View Model : Delete Command Added

--- MyHealth/ViewModels/TaskListViewModel.cs
+++ MyHealth/ViewModels/TaskListViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
 
 namespace MyHealth
 {
@@ -34,6 +35,7 @@ namespace MyHealth
         //Commands
         public TaskListAddTaskCommand AddTaskCommand { get; set; }
         public TaskListSaveCommand SaveCommand { get; set; }
+        public TaskListDeleteTaskCommand DeleteTaskCommand { get; set; }
 
         //ctor
         public TaskListViewModel()
@@ -43,6 +45,7 @@ namespace MyHealth
 
             AddTaskCommand = new TaskListAddTaskCommand(this);
             SaveCommand = new TaskListSaveCommand(this);
+            DeleteTaskCommand = new TaskListDeleteTaskCommand(this);
 
             TaskListDropHandler = new TaskListDropHandler();
         }
@@ -55,6 +58,11 @@ namespace MyHealth
             newTask.PropertyChanged += OnTaskChanged;
 
             TaskList.Add(newTask);
+            SaveTaskList();
+        }
+        public void DeleteSelectedTask()
+        {
+            TaskList.Remove(SelectedTask);
             SaveTaskList();
         }
 
@@ -83,6 +91,20 @@ namespace MyHealth
             context.CreateNewTask(parameter.ToString());
         }
     }
+    public class TaskListDeleteTaskCommand : ContextCommand<TaskListViewModel>
+    {
+        public TaskListDeleteTaskCommand(TaskListViewModel context) : base(context) { }
+
+        public override bool CanExecute(TaskListViewModel context, object parameter)
+        {
+            return context.SelectedTask != null;
+        }
+        public override void Execute(TaskListViewModel context, object parameter)
+        {
+            context.DeleteSelectedTask();
+        }
+    }
+
     public class TaskListSaveCommand : ContextCommand<TaskListViewModel>
     {
         public TaskListSaveCommand(TaskListViewModel context) : base(context) { }
