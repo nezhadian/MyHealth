518fcd9f6f332430d67eb543ba1e87b03457dbb7
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-07 16:30:35 +0430

    TimeSpanControl : Converterd To Control From UserControl

---
+++ MyHealth/Controls/Styles/TimeSpanControlStyle.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MyHealth
+{
+    public partial class TimeSpanControlStyle : ResourceDictionary
+    {
+        public TimeSpanControlStyle()
+        {
+            InitializeComponent();
+        }
+
+        //TextBoxes Filters
+        private void TextBoxOnlyNumbers_KeyDown(object sender, KeyEventArgs e)
+        {
+            int keyValue = (int)e.Key;
+            bool isInNumbpad = keyValue >= 74 && keyValue <= 83;
+            bool isInUpperNums = keyValue >= 34 && keyValue <= 43;
+            bool isBackSpace = e.Key == Key.Back;
+            bool isTab = e.Key == Key.Tab;
+            e.Handled = !(isInNumbpad || isInUpperNums || isBackSpace || isTab);
+        }
+        private void TextBoxFilterMinMaxNumber59_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.Text = int.TryParse(textBox.Text, out int n) ? Math.Max(Math.Min(n, 59), 0).ToString() : "0";
+        }
+        private void TextBoxFilterMinMaxNumber12_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.Text = int.TryParse(textBox.Text, out int n) ? Math.Max(Math.Min(n, 12), 0).ToString() : "0";
+        }
+        private void TextBoxForwardFocus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text.Length >= 2)
+            {
+                textBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next)
+                {
+                    Wrapped = true
+                });
+            }
+        }
+
+
+        //Select All When Click
+        private void TextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            (sender as TextBox).SelectAll();
+        }
+        private void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            if (!tb.IsKeyboardFocusWithin)
+            {
+                e.Handled = true;
+                tb.Focus();
+            }
+        }
+    }
+}
---
+++ MyHealth/Controls/TimeSpanControl.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyHealth
+{
+    /// <summary>
+    /// Interaction logic for TimeSpanControl.xaml
+    /// </summary>
+    public class TimeSpanControl : Control
+    {
+        #region Dp
+        public static readonly DependencyProperty TimeSpanProperty =
+            DependencyProperty.Register("TimeSpan", typeof(TimeSpan), typeof(TimeSpanControl), new PropertyMetadata(TimeSpan.Zero,OnTimeSpanChanged));
+        public static readonly DependencyProperty MinutesProperty =
+            DependencyProperty.Register("Minutes", typeof(int), typeof(TimeSpanControl), new PropertyMetadata(0, OnMinuteChanged));
+        public static readonly DependencyProperty SecondsProperty =
+            DependencyProperty.Register("Seconds", typeof(int), typeof(TimeSpanControl), new PropertyMetadata(0, OnSecondsChanged));
+        public static readonly DependencyProperty HoursProperty =
+            DependencyProperty.Register("Hours", typeof(int), typeof(TimeSpanControl), new PropertyMetadata(0, OnHoursChanged));
+
+
+        public static readonly DependencyProperty HourVisibilityProperty =
+            DependencyProperty.Register("HourVisibility", typeof(Visibility), typeof(TimeSpanControl), new PropertyMetadata(Visibility.Visible));
+        #endregion
+        public TimeSpan TimeSpan
+        {
+            get { return (TimeSpan)GetValue(TimeSpanProperty); }
+            set { SetValue(TimeSpanProperty, value); }
+        }
+
+        public int Minutes
+        {
+            get { return (int)GetValue(MinutesProperty); }
+            set { SetValue(MinutesProperty, value); }
+        }
+        public int Seconds
+        {
+            get { return (int)GetValue(SecondsProperty); }
+            set { SetValue(SecondsProperty, value); }
+        }
+        public int Hours
+        {
+            get { return (int)GetValue(HoursProperty); }
+            set { SetValue(HoursProperty, value); }
+        }
+
+        public Visibility HourVisibility
+        {
+            get { return (Visibility)GetValue(HourVisibilityProperty); }
+            set { SetValue(HourVisibilityProperty, value); }
+        }
+
+        //Static Dependency Properties Change Callbacks
+        private static void OnTimeSpanChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TimeSpanControl tsc = (TimeSpanControl)d;
+            TimeSpan timeSpan = (TimeSpan)e.NewValue;
+            tsc.Hours = timeSpan.Hours;
+            tsc.Minutes = timeSpan.Minutes;
+            tsc.Seconds = timeSpan.Seconds;
+        }
+
+        private static void OnMinuteChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TimeSpanControl tsc = (TimeSpanControl)d;
+            int minutes = (int)e.NewValue;
+            tsc.TimeSpan = new TimeSpan(tsc.Hours, minutes, tsc.Seconds);
+        }
+        private static void OnSecondsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TimeSpanControl tsc = (TimeSpanControl)d;
+            int seconds = (int)e.NewValue;
+            tsc.TimeSpan = new TimeSpan(tsc.Hours, tsc.Minutes, seconds);
+        }
+        private static void OnHoursChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TimeSpanControl tsc = (TimeSpanControl)d;
+            int hours = (int)e.NewValue;
+            tsc.TimeSpan = new TimeSpan(hours, tsc.Minutes, tsc.Seconds);
+        }
+    }
+}
