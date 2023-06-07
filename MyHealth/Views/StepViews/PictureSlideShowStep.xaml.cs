b3748a2009825212ef5330e6388d488138be97d0
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-07 18:46:25 +0430

    PictureSlideShowStep : Binded To ViewModel

--- MyHealth/Views/StepViews/PictureSlideShowStep.xaml.cs
+++ MyHealth/Views/StepViews/PictureSlideShowStep.xaml.cs
@@ -22,109 +22,11 @@ namespace MyHealth
     /// </summary>
     public partial class PictureSlideShowStep : Page
     {
-        #region Dp
-        public static readonly DependencyProperty ImageListProperty =
-            DependencyProperty.Register("ImageList", typeof(ObservableCollection<BitmapImage>), typeof(PictureSlideShowStep), new PropertyMetadata(new ObservableCollection<BitmapImage>()));
-        public static readonly DependencyProperty SelectedImageProperty =
-            DependencyProperty.Register("SelectedImage", typeof(BitmapImage), typeof(PictureSlideShowStep), new PropertyMetadata());
-        #endregion
-
-        public ObservableCollection<BitmapImage> ImageList
-        {
-            get { return (ObservableCollection<BitmapImage>)GetValue(ImageListProperty); }
-            set { SetValue(ImageListProperty, value); }
-        }
-        public BitmapImage SelectedImage
-        {
-            get { return (BitmapImage)GetValue(SelectedImageProperty); }
-            set { SetValue(SelectedImageProperty, value); }
-        }
-
-        private string imgDir;
-        public string ImagesDirectory
-        {
-            get { return imgDir; }
-            set { 
-                imgDir = value;
-                //Dirrectory Info
-                DirectoryInfo dirInfo = new DirectoryInfo(value);
-
-                //Check if exists
-                if (!dirInfo.Exists)
-                    throw new DirectoryNotFoundException();
-
-                //Get Images In Directory
-                ImageList = new ObservableCollection<BitmapImage>(
-                    dirInfo.EnumerateFiles().Select((i) => new BitmapImage(new Uri(i.FullName))));
-
-                //Set Properties
-                lstImages.SelectedIndex = FindRandomIndex();
-            }
-        }
-
-        DispatcherTimer timer;
-        Random r = new Random();
-
+        PictureSlideShowViewModel PictureSlideShowViewModel;
         public PictureSlideShowStep()
         {
-            timer = new DispatcherTimer
-            {
-                Interval = AppSettings.Data.ImageSliderDelay
-            };
-            timer.Tick += SlideShow_Tick;
-            
-            
-            DataContext = this;
             InitializeComponent();
         }
-
-        private void SlideShow_Tick(object sender, EventArgs e)
-        {
-            ComponentCommands.MoveRight.Execute(null,null);
-        }
-        
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            timer.Start();
-        }
-
-        private int FindNextIndex()
-        {
-            int selIndex = lstImages.SelectedIndex;
-            selIndex++;
-            if (selIndex >= ImageList.Count)
-                return 0;
-            else
-                return selIndex;
-        }
-        private int FindRandomIndex()
-        {
-            return r.Next(0, ImageList.Count);
-        }
-        private int FindPreviousIndex()
-        {
-            int selIndex = lstImages.SelectedIndex;
-            selIndex--;
-            if (selIndex < 0)
-                return ImageList.Count - 1;
-            else
-                return selIndex;
-        }
-
-        private void Always_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
-        }
-        private void NextCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            lstImages.SelectedIndex = FindNextIndex();
-        }
-        private void PreviousCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            lstImages.SelectedIndex = FindPreviousIndex();
-
-        }
-        
         public PictureSlideShowStep(StepData step)
             : this()
         {
@@ -132,13 +34,31 @@ namespace MyHealth
             switch (step.ImageList)
             {
                 case StepData.ImageListes.Body:
-                    ImagesDirectory = App.GetAssetsPath("Images","body");
+                    PictureSlideShowViewModel = new PictureSlideShowViewModel(App.GetAssetsPath("Images","body"));
                     break;
                 case StepData.ImageListes.Eye:
-                    ImagesDirectory = App.GetAssetsPath("Images","eye");
+                    PictureSlideShowViewModel = new PictureSlideShowViewModel(App.GetAssetsPath("Images", "eye"));
                     break;
             }
+            DataContext = PictureSlideShowViewModel;
+            PictureSlideShowViewModel.SelectionChanged += PictureSlideShowViewModel_SelectionChanged;
         }
 
+        private void PictureSlideShowViewModel_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            tmrSlider.Reset();
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnMoveRight.Focus();
+            tmrSlider.IsPaused = false;
+        }
+        private void tmrSlider_Completed(object sender, RoutedEventArgs e)
+        {
+            if (PictureSlideShowViewModel.NextCommand.CanExecute(null))
+            {
+                PictureSlideShowViewModel.NextCommand.Execute(null);
+            }
+        }
     }
 }
