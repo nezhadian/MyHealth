4fedddf47c9f0a5c924ccb6f59e380ae65f2b798
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-07 15:06:38 +0430

    Color2SolidColorBrush Converter Added

---
+++ MyHealth/Converters/Color2SolidColorBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MyHealth
+{
+    class Color2SolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Color clr)
+            {
+                return new SolidColorBrush(clr);
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SolidColorBrush brsh)
+            {
+                return brsh.Color;
+            }
+
+            return Binding.DoNothing;
+        }
+    }
+}
