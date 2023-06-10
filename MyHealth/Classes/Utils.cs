86abe4ce1e3951d9c21db6d0de3cee8647fe1189
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-10 19:24:24 +0430

    Utils : Label Setting  Added To YesNoMessageBox

--- MyHealth/Classes/Utils.cs
+++ MyHealth/Classes/Utils.cs
@@ -1,10 +1,21 @@
-﻿namespace MyHealth
+﻿using AdonisUI.Controls;
+namespace MyHealth
 {
     internal class Utils
     {
-        public static bool YesNoMessageBox(string caption, string message)
+        public static bool YesNoMessageBox(string caption, string message,string yesLabel = "Yes",string noLabel = "No")
         {
-            return AdonisUI.Controls.MessageBoxResult.Yes == AdonisUI.Controls.MessageBox.Show(message, caption, AdonisUI.Controls.MessageBoxButton.YesNo);
+            MessageBoxModel model = new MessageBoxModel();
+            model.Buttons = new MessageBoxButtonModel[]
+            {
+                new MessageBoxButtonModel(yesLabel,MessageBoxResult.Yes),
+                new MessageBoxButtonModel(noLabel,MessageBoxResult.No),
+            };
+
+            model.Text = message;
+            model.Caption = caption;
+            
+            return MessageBoxResult.Yes == MessageBox.Show(model);
         }
     }
 }
