15fb615e2786d89b330adcc5fb82dfe4cf020f94
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-10 18:23:09 +0430

    Enum2ArrayConverter Added

---
+++ MyHealth/Converters/Enum2ArrayConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MyHealth
+{
+
+    class Enum2ArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Type baseType)
+            {
+                Type targetEnumType = baseType.GetNestedType(parameter.ToString());
+
+                if (targetEnumType == null || !targetEnumType.IsEnum)
+                    return Binding.DoNothing;
+
+                return targetEnumType.GetEnumValues();
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
