36033abc13e8bb6fcb3b878d0e5fe3e86bfa87c2
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-11 08:15:56 +0430

    StepDataIconConverter : Renamed To StepData2IconCOnverter and Icons Prefix Added to Resource Addres

---
+++ MyHealth/Converters/StepData2IconConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Shapes;
+
+namespace MyHealth
+{
+
+    class StepData2IconConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values[0] is StepData.StepTypes stepType)
+            {
+                if(stepType != StepData.StepTypes.ImageSlider)
+                {
+                    return App.Current.Resources[$"Icons.StepData.{stepType}"];
+                }
+                else
+                {
+                    if (values[1] is StepData.ImageListes imageList)
+                    {
+                        return App.Current.Resources[$"Icons.StepData.{stepType}-{imageList}"];
+
+                    }
+                }
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
