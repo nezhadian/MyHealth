edd3aac468d01265d064859b8f6d7a1a3cce3d76
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-18 01:40:16 +0430

    MyObservableCollection with AddRange  Implemented

---
+++ MyHealth/ViewModels/MyObservableCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace MyHealth
+{
+    public class MyObservableCollection<T> : ObservableCollection<T>
+    {
+        public void AddRange(IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            CheckReentrancy();
+
+            foreach (var item in collection)
+            {
+                Items.Add(item);
+            }
+
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+    }
+}
