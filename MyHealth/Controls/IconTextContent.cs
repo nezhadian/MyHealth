134b7c0266a0e1c468ef67d93c3e0c1703b57ba5
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-21 19:48:08 +0430

    IconTextContent : Added

---
+++ MyHealth/Controls/IconTextContent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MyHealth
+{
+    public class IconTextContent:ContentControl
+    {
+
+
+        public Geometry IconData
+        {
+            get { return (Geometry)GetValue(IconDataProperty); }
+            set { SetValue(IconDataProperty, value);}
+        }
+
+        public static readonly DependencyProperty IconDataProperty =
+            DependencyProperty.Register("IconData", typeof(Geometry), typeof(IconTextContent), new PropertyMetadata());
+
+
+    }
+}
