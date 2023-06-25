d656221f518f4857ae057510920101e614c2a2b3
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-25 21:21:22 +0430

    Resource Dictionary Locator : Added

---
+++ MyHealth/Classes/ResourceDictionaryLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace MyHealth
+{
+    class ResourceDictionaryLocator
+    {
+        ResourceDictionary resourceDictionary;
+        string targetDirectory;
+
+        public virtual void ChangeFile(string fileName)
+        {
+            resourceDictionary.Source = new Uri(Path.Combine(targetDirectory,fileName),UriKind.Relative);
+        }
+
+        public ResourceDictionaryLocator(string defaultFilePath)
+        {
+            targetDirectory = Path.GetDirectoryName(defaultFilePath);
+            resourceDictionary = Application.Current.Resources.MergedDictionaries.First((rs) => rs.Source != null && rs.Source.OriginalString == defaultFilePath);
+        }
+
+        protected ResourceDictionaryLocator() { }
+    }
+}
