80198e51dd6fed43784779955e5ef3012fff76df
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-25 22:11:55 +0430

    SettingWindow : Inhert from adonis window

--- MyHealth/Windows/SettingsWindow.xaml.cs
+++ MyHealth/Windows/SettingsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdonisUI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,7 @@ namespace MyHealth
     /// <summary>
     /// Interaction logic for SettingsWindow.xaml
     /// </summary>
-    public partial class SettingsWindow : Window
+    public partial class SettingsWindow : AdonisWindow
     {
         public SettingsWindow()
         {
